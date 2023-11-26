ebc531e28794a9ccd7ece5413cbd09636d5e1b84
tej singh chaudhary <tej.bijay@gmail.com>
2023-11-26 11:56:49 +0545

    first commit

---
+++ index.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to our conference booking application")
+
+	conferenceName := "Go Conference"
+
+	fmt.Println(conferenceName)
+}
